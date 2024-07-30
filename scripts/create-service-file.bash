98396d072b36788b00cb1b0af42a4bcff1bcadc4
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-07-30 21:00:12 +0800

    feat: Update script to create more services

--- scripts/create-service-file.bash
+++ scripts/create-service-file.bash
@@ -5,13 +5,15 @@ CACHE_ROOT="${WORKD_DIR_ROOT}/.cache"
 
 # ETCD_NAME="arachne-node-alpha"
 INTERNAL_IP="192.168.0.144"
+KUBERNETES_PUBLIC_ADDRESS=127.0.0.1
 
 mkdir -p ${CACHE_ROOT}/services
+cd ${CACHE_ROOT}/services
 
-if [ -f ${CACHE_ROOT}/services/etcd.service ]; then
-    echo "File 'etcd.service' exist, skip."
+if [ -f etcd.service ]; then
+  echo "File 'etcd.service' exist, skip."
 else
-    cat <<EOF | tee ${CACHE_ROOT}/services/etcd.service
+  cat <<EOF | tee etcd.service
 [Unit]
 Description=etcd
 Documentation=https://etcd.io/
@@ -37,6 +39,105 @@ ExecStart=/usr/local/bin/etcd \\
   --initial-cluster controller=https://${INTERNAL_IP}:2380 \\
   --initial-cluster-state new \\
   --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+if [ -f kube-apiserver.service ]; then
+  echo "File 'kube-apiserver.service' exist, skip."
+else
+  cat >kube-apiserver.service <<EOF
+[Unit]
+Description=Kubernetes API Server
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-apiserver \\
+  --admission-control=Initializers,NamespaceLifecycle,NodeRestriction,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota \\
+  --advertise-address=${INTERNAL_IP} \\
+  --allow-privileged=true \\
+  --apiserver-count=3 \\
+  --audit-log-maxage=30 \\
+  --audit-log-maxbackup=3 \\
+  --audit-log-maxsize=100 \\
+  --audit-log-path=/var/log/audit.log \\
+  --authorization-mode=Node,RBAC \\
+  --bind-address=0.0.0.0 \\
+  --client-ca-file=/var/lib/kubernetes/ca.pem \\
+  # --enable-swagger-ui=true \\
+  --etcd-cafile=/var/lib/kubernetes/ca.pem \\
+  --etcd-certfile=/var/lib/kubernetes/kubernetes.pem \\
+  --etcd-keyfile=/var/lib/kubernetes/kubernetes-key.pem \\
+  --etcd-servers=https://${INTERNAL_IP}:2379 \\
+  --event-ttl=1h \\
+  --experimental-encryption-provider-config=/var/lib/kubernetes/encryption-config.yaml \\
+  --insecure-bind-address=127.0.0.1 \\
+  --kubelet-certificate-authority=/var/lib/kubernetes/ca.pem \\
+  --kubelet-client-certificate=/var/lib/kubernetes/kubernetes.pem \\
+  --kubelet-client-key=/var/lib/kubernetes/kubernetes-key.pem \\
+  --kubelet-https=true \\
+  --runtime-config=api/all \\
+  --service-account-key-file=/var/lib/kubernetes/ca-key.pem \\
+  --service-cluster-ip-range=10.32.0.0/24 \\
+  --service-node-port-range=30000-32767 \\
+  --tls-ca-file=/var/lib/kubernetes/ca.pem \\
+  --tls-cert-file=/var/lib/kubernetes/kubernetes.pem \\
+  --tls-private-key-file=/var/lib/kubernetes/kubernetes-key.pem \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+if [ -f kube-controller-manager.service ]; then
+  echo "File 'kube-controller-manager.service' exist, skip."
+else
+  cat >kube-controller-manager.service <<EOF
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-controller-manager \\
+  --address=0.0.0.0 \\
+  --cluster-cidr=10.200.0.0/16 \\
+  --cluster-name=kubernetes \\
+  --cluster-signing-cert-file=/var/lib/kubernetes/ca.pem \\
+  --cluster-signing-key-file=/var/lib/kubernetes/ca-key.pem \\
+  --leader-elect=true \\
+  --master=http://127.0.0.1:8080 \\
+  --root-ca-file=/var/lib/kubernetes/ca.pem \\
+  --service-account-private-key-file=/var/lib/kubernetes/ca-key.pem \\
+  --service-cluster-ip-range=10.32.0.0/24 \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+if [ -f kube-scheduler.service ]; then
+  echo "File 'kube-scheduler.service' exist, skip."
+else
+  cat >kube-scheduler.service <<EOF
+[Unit]
+Description=Kubernetes Scheduler
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-scheduler \\
+  --leader-elect=true \\
+  --master=http://127.0.0.1:8080 \\
+  --v=2
 Restart=on-failure
 RestartSec=5
 
